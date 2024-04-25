240eb575ea2e9c607072cdd43c5ece4f914ce342
Vrxshal <167960851+Vrxshal@users.noreply.github.com>
2024-04-25 16:22:45 +0000

    JobFinder

--- JobFinder.py
+++ JobFinder.py
@@ -8,29 +8,3 @@ def is_prime(num):
     for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             return False
-    return True
-
-number = int(input("Enter a number to check if it's prime: "))
-
-if is_prime(number):
-    print(number, "is a prime number.")
-else:
-    print(number, "is not a prime number.")
-
-else:
-    print(number, "is not a prime number.")
-number = int(input("Enter a number to check if it's prime: "))
-
-if is_prime(number):
-    print(number, "is a prime number.")
-else:
-    print(number, "is not a prime number.")
-
-else:
-    print(number, "is not a prime number.")
-  print(number, "is a prime number.")
-else:
-    print(number, "is not a prime number.")
-
-else:
-    print(number, "is not a prime number.")
