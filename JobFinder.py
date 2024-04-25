1a08bfacca3ebb26b45fd182b6297b2282d06bb0
Vrishal <vrielan@gmail.com>
2024-04-25 23:47:50 +0800

    First commit

--- JobFinder.py
+++ JobFinder.py
@@ -1,9 +1,4 @@
-def calculate_area(length, width):
-    area = length * width
-    return area
+from new_func import new_func
 
-length = float(input("Enter the length of the rectangle: "))
-width = float(input("Enter the width of the rectangle: "))
 
-area = calculate_area(length, width)
-print("The area of the rectangle is:", area)
+return new_func()
