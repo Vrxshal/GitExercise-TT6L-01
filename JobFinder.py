a9d0ae0e1e48ee4d18ae603b5690f78613b88cb7
Vrxshal <167960851+Vrxshal@users.noreply.github.com>
2024-04-25 16:12:40 +0000

    First commit

--- JobFinder.py
+++ JobFinder.py
@@ -2,3 +2,20 @@ from new_func import new_func
 
 
 return new_func()
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+number = int(input("Enter a number to check if it's prime: "))
+
+if is_prime(number):
+    print(number, "is a prime number.")
+else:
+    print(number, "is not a prime number.")
+
+else:
+    print(number, "is not a prime number.")
