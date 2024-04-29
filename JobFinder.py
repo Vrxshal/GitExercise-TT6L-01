801d742a50fa6a1bfa778b8c8528c7172a12a586
Vrxshal <167960851+Vrxshal@users.noreply.github.com>
2024-04-29 10:16:22 +0000

    Job Finder

--- JobFinder.py
+++ JobFinder.py
@@ -1,10 +1,19 @@
-from new_func import new_func
-
-
-return new_func()
-def is_prime(num):
-    if num <= 1:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
+def register():
+    names=[]
+    usernames=[]
+    passwords=[]
+    names.append(input("Enter your name:"))
+    usernames.append(input("choose your username:"))
+    passwords.append(input("choose your password:"))
+    return usernames
+def login(usernames,passwords):
+    usernames=[]
+    passwords=[]
+    password=""
+    username=""
+    username=input("Enter your username:")
+    password=input("Enter your Password:")
+    if password==passwords[usernames.index(username)]:
+       print("welcome")
+    else:
+       print("incorrect!")
